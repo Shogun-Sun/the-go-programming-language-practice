018b2aa28d16df12fc0ea5932ca0d42cfa16421b
Shogun_Sun <fima8516@gmail.com>
2026-06-08 11:23:38 +0300

    start part 2, complete 2.1-2.2 subsections

---
+++ ch2/announcements/boiling/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	boilingF = 943.0
+)
+
+func main() {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Printf("Температура кипения = %g°F или %g°C\n", f, c)
+}
---
+++ ch2/announcements/ftoc/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const (
+		freezingF, boilinF = 32.0, 212.0
+	)
+
+	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF))
+
+	fmt.Printf("%g°F = %g°C\n", boilinF, fToC(boilinF))
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
